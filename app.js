31b9adce195337633d8fb39d47bc9a8564cac4ad
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-28 10:39:41 +0700

    Display user function finished

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
+import displayUser from "./utils/displayUser.js";
 import getUser from "./utils/fetchUser.js";
 import getElement from "./utils/getElement.js";
 
-const img = getElement(".user-img");
-const title = getElement(".user-title");
-const value = getElement(".user-value");
 const btn = getElement(".btn");
-const btns = [...document.querySelectorAll(".icon")];
 
 const showUser = async () => {
   //   get user from api
   const person = await getUser();
-  console.log(person);
   // display user
+  displayUser(person);
 };
 
 window.addEventListener("DOMContentLoaded", showUser);
