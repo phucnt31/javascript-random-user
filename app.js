a2d79b9fc3b6baca43745aa3426c7f118961b5b8
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-28 10:25:28 +0700

    Fetch user

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+import getUser from "./utils/fetchUser.js";
 import getElement from "./utils/getElement.js";
-
-const URL = "https://randomuser.me/api/";
 
 const img = getElement(".user-img");
 const title = getElement(".user-title");
 const value = getElement(".user-value");
 const btn = getElement(".btn");
 const btns = [...document.querySelectorAll(".icon")];
+
+const showUser = async () => {
+  //   get user from api
+  const person = await getUser();
+  console.log(person);
+  // display user
+};
+
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
